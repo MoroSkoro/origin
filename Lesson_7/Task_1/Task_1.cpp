55b1998b440a2c766f9bcc8d1aefc2a91c87e315
MoroSkoro <evgenij-kapshuk@yandex.ru>
2023-02-13 13:03:41 +0300

    Added Lesson_7

---
+++ Lesson_7/Task_1/Task_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#define MODE '1'
+
+double add(double a, double b) { return (a + b); }
+
+int main()
+{
+    //SetConsoleCP(1251);
+     //SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    
+    char mode;
+    mode = MODE;
+    double a, b;
+
+#ifndef MODE
+#error Необходимо определить MODE
+#endif // !MODE
+
+#if MODE=='0'
+    std::cout << "«Работаю в режиме тренировки»";
+#elif MODE=='1'
+    std::cout << "«Работаю в боевом режиме»\n";
+    std::cout << "«Введите число 1: ";
+    std::cin >> a;
+    std::cout << "«Введите число 2: ";
+    std::cin >> b;
+    std::cout << "«Результат сложения: " << add(a, b) << std:: endl;
+#else
+    std::cout << "«Неизвестный режим. Завершение работы»";
+#endif
+
+}
