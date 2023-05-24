fcd2a26a67bfb4f9d3e2b987ca7046bd3b2bbf4d
MoroSkoro <evgenij-kapshuk@yandex.ru>
2023-05-25 00:50:41 +0300

    Added Lesson_7

---
+++ Advanced programming/Lesson_7/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main(){
+	std::vector<int> vec{1, 1, 2, 5, 6, 1, 2, 4 };
+	std::cout << "[IN]: ";
+	for_each(vec.begin(), vec.end(), [](const int& n) {std::cout << n << " "; });
+	std::cout << "\n";
+	
+	sort(vec.begin(), vec.end());
+	std::vector<int>::iterator it;
+	it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+	std::cout << "[OUT]: ";
+	//copy(vec.begin(), vec.end(), std::ostream_iterator(std::cout, " "));
+	for_each(vec.begin(), vec.end(), [](const int& n) {std::cout << n << " "; });
+	
+	std::cout << "\n\n";
+	system("pause");
+}
---
+++ Advanced programming/Lesson_7/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <stdio.h>
+#include <stdarg.h>
+
+template<typename T>
+void print_container(T& a) {
+	for (auto i : a) std::cout << i << " ";
+	std::cout << "\n";
+}
+
+int main() {
+	
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+	
+	std::cout << "\n\n";
+	system("pause");
+}
