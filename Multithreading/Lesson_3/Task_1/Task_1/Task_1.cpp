7919c0c6ae71c4cf69af2b8dab6ffb8648b72f9d
MoroSkoro <evgenij-kapshuk@yandex.ru>
2023-07-31 19:51:09 +0300

    Added Lesson_3

---
+++ Multithreading/Lesson_3/Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+#include <list>
+#include <future>
+#include <iterator>
+
+std::list<int>::iterator ThreadFunc (std::list<int>& c1, std::list<int>::iterator& it_start, int& min,
+    std::list<int>::iterator& it_min) {
+    std::list<int>::iterator iter;
+    for (iter = it_start; iter != c1.end(); iter++) {
+        if (min >= *iter) {
+            min = *iter;
+            it_min = iter;
+        }
+    }
+    return it_min;
+}
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "ru_RU.1251");
+    
+    std::list<int> c1{5, 3, -1, 7, 8, 89, 0};
+
+    std::list<int>::iterator it_start, it1, iter, it_min;
+    it_start = c1.begin();
+    int min = *it_start;
+    int size = c1.size();
+
+    std::cout << "До сортировки: " << std::endl;
+    for (auto c : c1) {
+        std::cout << c << "  ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < size; i++) {
+        int min = *it_start;
+
+        auto ft = std::async(std::launch::async, ThreadFunc, std::ref(c1), std::ref(it_start),
+            std::ref(min), std::ref(it_min));
+
+        ft.wait();
+        it_min = ft.get();
+       
+        if (it_start != it_min) {
+            *it_min = *it_start;
+            *it_start = min;
+        }
+        ++it_start;
+    }
+
+    std::cout <<"После сортировки: "<<std::endl;
+    for (auto c : c1) {
+        std::cout << c << "  ";
+
+    }
+    std::cout << std::endl;
+    return 0;
+}
---
+++ Multithreading/Lesson_3/Task_2/Task_2/Task_2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+
+template<typename Iterator, typename Func>
+
+void parallel_for_each(Iterator first, Iterator last, unsigned long const &min_per_thread, Func f) {
+
+    unsigned long const length = std::distance(first, last);
+
+    if (!length) return;
+
+    if (length < (2 * min_per_thread)) {
+        std::for_each(first, last, f);
+    }
+    else {
+
+        Iterator const mid_point = first + length / 2;
+
+        std::future<void> first_half =
+
+            std::async(std::launch::async, &parallel_for_each<Iterator, Func>, first, mid_point, min_per_thread, f);
+
+        parallel_for_each(mid_point, last, min_per_thread, f);
+
+        first_half.get();
+    }
+}
+
+std::mutex m;
+
+template<typename T>
+void f1(T& x) {
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
+    x= x* 7;
+    std::unique_lock<std::mutex> guard(m);
+    std::cout << x << " ";
+}
+
+
+int main(){
+    
+    unsigned long min_per_thread = 8;
+    size_t size = 30;
+    std::vector<double> v(size, 1);
+    
+    void(*poter)(double& v) = nullptr;
+    poter = &f1;
+
+    for (size_t i = 0; i < size; i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    parallel_for_each(v.begin(), v.end(), min_per_thread, *poter);
+    std::cout << std::endl;
+}
