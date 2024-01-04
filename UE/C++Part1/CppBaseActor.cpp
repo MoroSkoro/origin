dd2ca5ab475a02c087e0bdcbae2a3829caa8fb5d
MoroSkoro <evgenij-kapshuk@yandex.ru>
2024-01-04 04:59:14 +0300

    Added Task1

---
+++ UE/C++Part1/CppBaseActor.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CppBaseActor.h"
+#include "Engine/Engine.h"
+
+// Sets default values
+ACppBaseActor::ACppBaseActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void ACppBaseActor::BeginPlay()
+{
+	Super::BeginPlay();
+	ShowActorInformation();
+}
+
+// Called every frame
+void ACppBaseActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACppBaseActor::ShowActorInformation() {
+	UE_LOG(LogTemp, Display, TEXT("Instance name: %s"), *PlayerName);
+	UE_LOG(LogTemp, Display, TEXT("EnemyNum: %d"), EnemyNum);
+	UE_LOG(LogTemp, Display, TEXT("IsAlive: %i"), IsAlive);
+}
+
+
